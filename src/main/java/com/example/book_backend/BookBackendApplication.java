0e9a047457bcba16d6d0d26ead8bf23386281411
sinemnedzhati-2002 <sinemnedzahti16@gmail.com>
2025-11-20 15:13:05 +0100

    Inital Spring Boot Backend

---
+++ src/main/java/com/example/book_backend/BookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBackendApplication.class, args);
+	}
+
+}
